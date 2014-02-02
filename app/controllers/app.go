84476cda9d2def0f6af66f84eec7fc41c1faff6f
Xabier Larrakoetxea <slok69@gmail.com>
2014-02-02 16:29:01 +0100

    Added basic image creation controller

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,15 +1,55 @@
 package controllers
 
 import (
-	"fmt"
+	"net/http"
 
+	"code.google.com/p/freetype-go/freetype/truetype"
 	"github.com/robfig/revel"
+	"github.com/slok/gummyimage"
 )
 
 type Application struct {
 	*revel.Controller
 }
 
+type ImageResponse struct {
+	sizeX   int
+	sizeY   int
+	bgColor string
+	fgColor string
+	text    string
+	format  string
+}
+
+// Global variable
+var (
+	font *truetype.Font
+)
+
+// Custom responses -----------------------------------------------------------
+// Custom response for image
+func (r ImageResponse) Apply(req *revel.Request, resp *revel.Response) {
+
+	// FIX:
+	// If settings loaded out of actions then revel throws nil pointer, so we
+	// load here the first time only
+	if font == nil {
+		fontPath, _ := revel.Config.String("gummyimage.fontpath")
+		font, _ = gummyimage.LoadFont(fontPath)
+	}
+
+	resp.WriteHeader(http.StatusOK, "image/png")
+
+	//TODO: Random color for now
+	g, _ := gummyimage.NewDefaultGummy(r.sizeX, r.sizeY)
+	g.Font = font
+	g.DrawTextSize(r.fgColor)
+
+	b, _ := g.GetPng()
+	resp.Out.Write(b)
+}
+
+// Actions --------------------------------------------------------------------
 func (c Application) Index() revel.Result {
 	return c.Render()
 }
@@ -18,13 +58,9 @@ func (c Application) CreateImage() revel.Result {
 
 	// Get params by dict because we use this action for 3 different url routes
 	// with different url params
-	size := c.Params.Get("size")
-	bgColor := c.Params.Get("bgcolor")
-	fgColor := c.Params.Get("fgcolor")
+	//size := c.Params.Get("size")
+	//bgColor := c.Params.Get("bgcolor")
+	//fgColor := c.Params.Get("fgcolor")
 
-	fmt.Println(size)
-	fmt.Println(bgColor)
-	fmt.Println(fgColor)
-
-	return c.Render()
+	return ImageResponse(ImageResponse{600, 600, "", "", "", "png"})
 }
